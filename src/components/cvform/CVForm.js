79efcf1981df50586e4c2ba57780627dc55b4daf
KLIM STOROZHENKO <storozhenko.klim@gmail.com>
2022-10-18 10:46:34 +1000

    Create CVForm component

---
+++ src/components/cvform/CVForm.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import PersonalInfo from "./PersonalInfo";
+
+class CVForm extends Component {
+    render() {
+        return (<div className="CVForm">  
+            <PersonalInfo />
+        </div>);
+    }
+}
+
+export default CVForm;
