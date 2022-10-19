e5119ac4d8f46c1faac7488b80bd2c684b6b59e6
KLIM STOROZHENKO <storozhenko.klim@gmail.com>
2022-10-19 11:00:57 +1000

    Add CVForm render

--- src/components/cvform/CVForm.js
+++ src/components/cvform/CVForm.js
@@ -1,10 +1,14 @@
 import React, { Component } from "react";
+import Experience from "./Experience";
 import PersonalInfo from "./PersonalInfo";
+import Education from "./Education";
 
 class CVForm extends Component {
     render() {
         return (<div className="CVForm">  
-            <PersonalInfo />
+            <PersonalInfo info={this.props.cv.personal} />
+            <Experience data={this.props.cv.experience} />
+            <Education data={this.props.cv.education} />
         </div>);
     }
 }
