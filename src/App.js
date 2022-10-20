95a553bbab8023f954899d9fe5d7f62ededa5751
KLIM STOROZHENKO <storozhenko.klim@gmail.com>
2022-10-20 12:40:59 +1000

    Create state and personal change handler

--- src/App.js
+++ src/App.js
@@ -3,8 +3,31 @@ import Main from "./components/Main";
 import cv from './modules/cv-example';
 
 class App extends Component {
+  state = {
+    cv: cv
+  }
+
+
+  handlePersonalChange = (e) => {
+    const { name, value } = e.target;
+    this.setState(prevState => {
+      let newCV = Object.assign({}, prevState.cv);
+      newCV.personal[name] = value;
+      return newCV;
+    })
+  }
+
+  handleEducationChange = () => {
+    console.log('ed');
+  }
+    
+
   render() {
-    return (<Main cv={cv} />);
+    return (<Main
+      cv={this.state.cv}
+      handlePersonalChange={this.handlePersonalChange}    
+      handleEducationChange={this.handleEducationChange}
+    />);
   }
 }
 
