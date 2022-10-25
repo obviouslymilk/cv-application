7ea3c89cefbd41bac70c5278684a884216d09f24
KLIM STOROZHENKO <storozhenko.klim@gmail.com>
2022-10-25 11:06:42 +1000

    Updates

---
+++ precache-manifest.c1ad54218b6f56b0a707ee8fb1f5de52.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a983aa665dae6e91b7cd",
+    "url": "/cv-application/static/css/main.726a4279.chunk.css"
+  },
+  {
+    "revision": "a983aa665dae6e91b7cd",
+    "url": "/cv-application/static/js/main.a983aa66.chunk.js"
+  },
+  {
+    "revision": "388838b6a218469360d3",
+    "url": "/cv-application/static/js/1.388838b6.chunk.js"
+  },
+  {
+    "revision": "65881043a4b24edec006",
+    "url": "/cv-application/static/js/runtime~main.65881043.js"
+  },
+  {
+    "revision": "a0b9c8dd5bbe0171a3d0927785a97a45",
+    "url": "/cv-application/index.html"
+  }
+];
