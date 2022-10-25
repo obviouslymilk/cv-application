2684be235797be638cac0ec7c86ee66184bbb52e
KLIM STOROZHENKO <storozhenko.klim@gmail.com>
2022-10-25 11:11:56 +1000

    Add GitHub link to the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 class Footer extends Component {
     render() {
         return (<footer>
-            github link
+            <a href="https://github.com/obviouslymilk/cv-application">GitHub</a>
         </footer>);
     }
 }
