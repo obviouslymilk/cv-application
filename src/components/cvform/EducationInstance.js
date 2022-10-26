7fdf0fc7711452c3be295d4246e78d9312cd9125
KLIM STOROZHENKO <storozhenko.klim@gmail.com>
2022-10-26 11:52:37 +1000

    Refactor Form instaces to functional components

--- src/components/cvform/EducationInstance.js
+++ src/components/cvform/EducationInstance.js
@@ -1,18 +1,17 @@
-import React, { Component } from "react";
+import React from "react";
 import Input from "../utils/Input";
 
-class EducationInstance extends Component {
-    render() {
-        const { from, to, title, location } = this.props.data;
-        const onChange = this.props.onChange;
-        return (<div id={this.props.id} className="instance">
-            <Input type="text" name="from" placeholder="From" value={from} onChange={onChange} />
-            <Input type="text" name="to" placeholder="To" value={to} onChange={onChange} />
-            <Input type="text" name="title" placeholder="Degree" value={title} onChange={onChange} />
-            <Input type="text" name="location" placeholder="University" value={location} onChange={onChange} />
-            <button onClick={this.props.onDelete}>Delete</button>
-        </div>);
-    }
+function EducationInstance(props) {
+    const { from, to, title, location } = props.data;
+    const onChange = props.onChange;
+
+    return <div id={props.id} className="instance">
+        <Input type="text" name="from" placeholder="From" value={from} onChange={onChange} />
+        <Input type="text" name="to" placeholder="To" value={to} onChange={onChange} />
+        <Input type="text" name="title" placeholder="Degree" value={title} onChange={onChange} />
+        <Input type="text" name="location" placeholder="University" value={location} onChange={onChange} />
+        <button onClick={props.onDelete}>Delete</button>
+    </div>;
 }
 
 export default EducationInstance;
--- src/components/cvform/ExperienceInstance.js
+++ src/components/cvform/ExperienceInstance.js
@@ -1,19 +1,18 @@
-import React, { Component } from "react";
+import React from "react";
 import Input from "../utils/Input";
 
-class ExperienceInstance extends Component {
-    render() {
-        const { from, to, position, company, location } = this.props.data;
-        const onChange = this.props.onChange;
-        return (<div id={this.props.id} className="instance">
-            <Input type="text" name="from" placeholder="From" value={from} onChange={onChange}  />
-            <Input type="text" name="to" placeholder="To" value={to} onChange={onChange}  />
-            <Input type="text" name="position" placeholder="Position" value={position} onChange={onChange}  />
-            <Input type="text" name="company" placeholder="Company Name" value={company} onChange={onChange}  />
-            <Input type="text" name="location" placeholder="Location" value={location} onChange={onChange}  />
-            <button onClick={this.props.onDelete}>Delete</button>
-        </div>);
-    }
+function ExperienceInstance(props) {
+    const { from, to, position, company, location } = props.data;
+    const onChange = props.onChange;
+    
+    return <div id={props.id} className="instance">
+        <Input type="text" name="from" placeholder="From" value={from} onChange={onChange}  />
+        <Input type="text" name="to" placeholder="To" value={to} onChange={onChange}  />
+        <Input type="text" name="position" placeholder="Position" value={position} onChange={onChange}  />
+        <Input type="text" name="company" placeholder="Company Name" value={company} onChange={onChange}  />
+        <Input type="text" name="location" placeholder="Location" value={location} onChange={onChange}  />
+        <button onClick={props.onDelete}>Delete</button>
+    </div>;  
 }
 
 export default ExperienceInstance;
